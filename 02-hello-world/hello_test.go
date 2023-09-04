ae733e22f4865b01af3a48b5fb3a6d81a2c5e6a0
Parkreiner <throwawayclover@gmail.com>
2023-09-04 11:23:58 -0400

    minor: Update tests for hello world

--- 02-hello-world/hello_test.go
+++ 02-hello-world/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	result := Hello("Bob Bobson")
-	expected := "Hello, Bob Bobson!"
+	t.Run("Says hello to people", func(t *testing.T) {
+		result := Hello("Bob Bobson")
+		expected := "Hello, Bob Bobson!"
 
-	if result != expected {
-		t.Errorf("got %q, but want %q", result, expected)
-	}
+		if result != expected {
+			t.Errorf("got %q, but want %q", result, expected)
+		}
+	})
+
+	t.Run("Says 'Hello, world!' when an empty string is provided", func(t *testing.T) {
+		result := Hello("")
+		expected := "Hello, world!"
+
+		if result != expected {
+			t.Errorf("got %q, but want %q", result, expected)
+		}
+	})
 }
